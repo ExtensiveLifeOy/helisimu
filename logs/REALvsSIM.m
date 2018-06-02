567d934b5b8173889bf7b3ccca0335c002ced6be
Luis Diago <ldiago@gmail.com>
2018-06-03 03:33:48 +0900

    Add graph for comparisson of two models (BRAIDFORDvsPROPOSED)

--- logs/REALvsSIM.m
+++ logs/REALvsSIM.m
@@ -2,19 +2,25 @@ clear all
 close all
 clc
 load REALvsSIM
+load ADH365m
 figure, subplot(2,1,1),
 histogram(TReal.ADH, 'FaceColor', 'b')
 hold on, histogram(TSim.ADH,'FaceColor','r')
-legend('real','simulation')
+hold on, histogram(ADH365m01,'FaceColor','g')
+legend('real','Braidford','Proposed')
 subplot(2,1,2)
 pdr = fitdist(TReal.ADH,'Kernel','Kernel','epanechnikov');
 x_values = 0:0.005:1;
 yr = pdf(pdr,x_values);
 plot(x_values,yr)
-pds = fitdist(TSim.ADH,'Kernel','Kernel','epanechnikov');
-ys = pdf(pds,x_values);
+pdb = fitdist(TSim.ADH,'Kernel','Kernel','epanechnikov');
+yb = pdf(pdb,x_values);
 hold on
-plot(x_values,ys,'r')
+plot(x_values,yb,'r')
+pdp = fitdist(ADH365m01,'Kernel','Kernel','epanechnikov');
+yp = pdf(pdp,x_values);
+hold on
+plot(x_values,yp,'g')
 ylabel('PDF(adh)')
 xlabel('adh')
-legend('real','simulation')
+legend('real','Braidford','Proposed')
